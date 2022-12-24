9e32e9d04f504c9b8db3cf9faa50d43c49c33789
Uditanshu Satpathy <uditanshusatpathy23@gmail.com>
2022-12-24 23:45:12 +0530

    swagger docs updated

--- app.py
+++ app.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 import inference
 from utils.inference import Inference
 
+description = '''
+# Sentiment Analysis app using FastAPI
+
+it is a simple app to predict the sentiment of the tweet using tensorflow sequential model
+'''
+
 inference = inference.main()
-app = FastAPI()
+app = FastAPI(
+    title='Sentiment Analysis',
+    description=description,
+    version='0.0.1',
+    license_info={
+        'name': 'MIT',
+        'url': 'https://www.mit.edu/~amini/LICENSE.md',
+    }
+)
 
 @app.get('/')
 async def index() -> str:
@@ -12,4 +26,4 @@ async def index() -> str:
 @app.get('/predict')
 async def predict_sentiment(text: str) -> str:
     result = inference.predict_sentiment(text)
-    return result
+    return {'text': text, 'sentiment': result}
