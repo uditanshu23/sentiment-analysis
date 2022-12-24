73743cbbdec42c730224e6a5a17d422cada10c69
Uditanshu Satpathy <uditanshusatpathy23@gmail.com>
2022-12-24 13:29:03 +0530

    feat: training pipeline

---
+++ training.py
@@ -0,0 +1,22 @@
+# -- coding: utf-8 --
+'''
+Created on 24-12-2022 13:24
+Project : sentiment-analysis
+@author : Uditanshu Satpathy
+@emails : uditanshusatpathy23@gmail.com
+'''
+
+import os
+
+from utils.processor import *
+from utils.train import *
+
+def main():
+    path = os.path.join(os.getcwd(), 'data', 'airline_sentiment_analysis.csv')
+    processor = Processor()
+    train_df, test_df = processor.data_processor(path)
+    train = Train()
+    train.train(train_df)
+
+if __name__ == '__main__':
+    main()
