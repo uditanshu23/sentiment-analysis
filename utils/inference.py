4e5ecf9847755e394e5c38f503ec09b61d2da407
Uditanshu Satpathy <uditanshusatpathy23@gmail.com>
2022-12-24 17:51:15 +0530

    inference function updated for api call

--- utils/inference.py
+++ utils/inference.py
@@ -16,7 +16,7 @@ from tensorflow.keras.layers import Embedding
 
 class Inference:
 
-    def __init__(self, checkpoint_path = "sentiment_analysis/cp-{epoch:04d}.ckpt"):
+    def __init__(self, checkpoint_path = "models/cp-{epoch:04d}.ckpt"):
         self.checkpoint_path = checkpoint_path
 
     def create_model(vocab_size, embedding_vector_length=32, loss='binary_crossentropy', optimizer='adam'):
@@ -26,24 +26,24 @@ class Inference:
         model.add(LSTM(50, dropout=0.5, recurrent_dropout=0.5))
         model.add(Dropout(0.2))
         model.add(Dense(1, activation='sigmoid')) 
-        model.compile(loss, optimizer, metrics=['accuracy'])  
+        model.compile(optimizer, loss, metrics=['accuracy'])  
         print(model.summary()) 
         return model
 
-    def predict_sentiment(tokenizer, senti_lab, model, text):
+    def predict_sentiment(self, text):
         
-        tw = tokenizer.texts_to_sequences([text])
+        tw = self.tokenizer.texts_to_sequences([text])
         tw = pad_sequences(tw,maxlen=200)
-        prediction = int(model.predict(tw).round().item())
-        print("Predicted label: ", senti_lab[1][prediction])
+        prediction = int(self.model.predict(tw).round().item())
+        # print("Predicted label: ", self.senti_lab[1][prediction])
 
-        return None
+        return self.senti_lab[1][prediction]
 
     def inference(self,train_df, test_df):
 
         senti_lab = train_df.airline_sentiment.factorize()
 
-        text = test_df.text.values
+        text = train_df.text.values
         tokenizer = Tokenizer(num_words=5000)
         tokenizer.fit_on_texts(text)
         encoded_docs = tokenizer.texts_to_sequences(text)
@@ -52,9 +52,12 @@ class Inference:
 
         checkpoint_dir = os.path.dirname(self.checkpoint_path)
         latest = tf.train.latest_checkpoint(checkpoint_dir)
-        model.load_weights(latest)
+        model.load_weights(latest).expect_partial()
 
-        self.predict_sentiment(tokenizer, senti_lab, model, text)
+        self.tokenizer = tokenizer
+        self.senti_lab = senti_lab
+        self.model = model
+
+        # self.predict_sentiment('Hello, fuck you!')
 
         return None
-
